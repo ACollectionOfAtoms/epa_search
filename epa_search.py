f833b293023ce358f666832f78167a2818b68aa7
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-04-06 17:21:25 -0500

    Added State Wide Comparison.

--- epa_search.py
+++ epa_search.py
@@ -87,15 +87,46 @@ class App(wx.App):
                 dlg2.Destroy()
                 dlg2.EndModal(dlg2.GetReturnCode())
                 self.query(None)
-                
+
+
+
+        if response == 'State Wide Analysis':
+            #SQL to get list of states from EPA database
+            cur.execute('select distinct FAC_STATE from frs_fac order by fac_state')
+            info = cur.fetchall()
+
+            #Convert list of tuples to list of strings
+            info = ["%s" % x for x in info]
+
+            #Put list of strings into new dialog box, for user selection.
+            dlg2 = wx.SingleChoiceDialog(None,'Select State', 'Query Construction',
+                                    info)
+
+            if dlg2.ShowModal() == wx.ID_OK:
+                state = dlg2.GetStringSelection()
+                dlg2.EndModal(dlg2.GetReturnCode())
+                dlg2.Destroy()
+                self.results(None,state)
+
+
+
+    
         else:
             self.query(None)
+        return None
         
     def results(self,city,state):
-        #SQL Query
-        sql = 'select * from (select i.fac_name,(coalesce(j.violations, 0) + coalesce(k.violations, 0) + coalesce(l.violations, 0)) as total from frs_fac i full join h2o j on i.fac_name = j.fac_name and i.fac_street = j.fac_street and i.fac_city = j.city and i.fac_state = j.state_code full join aire k on i.fac_name = k.fac_name and i.fac_street = k.fac_street and i.fac_city = k.fac_city and i.fac_state = k.fac_state full join haz l on i.fac_name = l.fac_name and i.fac_street = l.fac_street and i.fac_city = l.fac_city and i.fac_state = l.fac_state where i.fac_city =:c and i.fac_state =:s order by total desc) where total > 0'
-        cur.execute(sql, c=city, s=state)
-        self.res = cur.fetchall()
+        #SQL Query, check if given a city
+        if city == None:
+            self.state = True
+            sql = 'select fac_city, sum(total) as CityTotal from (select i.fac_city, (coalesce(j.violations, 0) + coalesce(k.violations, 0) + coalesce(l.violations, 0)) as total from frs_fac i full join h2o j on i.fac_name = j.fac_name and i.fac_street = j.fac_street and i.fac_city = j.city and i.fac_state = j.state_code full join aire k on i.fac_name = k.fac_name and i.fac_street = k.fac_street and i.fac_city = k.fac_city and i.fac_state = k.fac_state full join haz l on i.fac_name = l.fac_name and i.fac_street = l.fac_street and i.fac_city = l.fac_city and i.fac_state = l.fac_state where i.fac_state =:s order by total desc) group by fac_city order by citytotal desc'
+            cur.execute(sql, s=state)
+            self.state_res = cur.fetchall()
+
+        else:    
+            sql = 'select * from (select i.fac_name,(coalesce(j.violations, 0) + coalesce(k.violations, 0) + coalesce(l.violations, 0)) as total from frs_fac i full join h2o j on i.fac_name = j.fac_name and i.fac_street = j.fac_street and i.fac_city = j.city and i.fac_state = j.state_code full join aire k on i.fac_name = k.fac_name and i.fac_street = k.fac_street and i.fac_city = k.fac_city and i.fac_state = k.fac_state full join haz l on i.fac_name = l.fac_name and i.fac_street = l.fac_street and i.fac_city = l.fac_city and i.fac_state = l.fac_state where i.fac_city =:c and i.fac_state =:s order by total desc) where total > 0'
+            cur.execute(sql, c=city, s=state)
+            self.res = cur.fetchall()
 
         #Initialize frame!
         self.top = wx.Frame(None, wx.ID_ANY, title='Results', size=(580,200))
@@ -107,14 +138,20 @@ class App(wx.App):
 
         #Specify the panels' size, and titles
         self.top.list_ctrl = wx.ListCtrl(self.panel, size=(-1,100), style=wx.LC_REPORT | wx.BORDER_SUNKEN)
-        self.top.list_ctrl.InsertColumn(0, 'Facility Name')
-        self.top.list_ctrl.InsertColumn(1, 'Number Of Violations')
+        if city == None:
+            self.top.list_ctrl.InsertColumn(0, 'City')
+            self.top.list_ctrl.InsertColumn(1, 'Total Violations')
+
+        else:
+            self.top.list_ctrl.InsertColumn(0, 'Facility Name')
+            self.top.list_ctrl.InsertColumn(1, 'Number Of Violations')
+
         self.top.list_ctrl.SetColumnWidth(0,350)
         self.top.list_ctrl.SetColumnWidth(1,150)
 
         #Add some buttons...
         btn1 = wx.Button(self.panel, label="OK")
-        btn2 = wx.Button(self.panel, label="Generate Graph of Data In Current Folder")
+        btn2 = wx.Button(self.panel, label="Generate Graph of Top 10 EPA Violators In Current Folder")
 
         #Button events
         btn1.Bind(wx.EVT_BUTTON, self.resClose)
@@ -127,41 +164,71 @@ class App(wx.App):
         self.sizer.Add(btn2, 0, wx.ALL|wx.CENTER,8)
         self.panel.SetSizer(self.sizer)
         #populate table
-        for i in self.res:
-            self.top.list_ctrl.InsertStringItem(self.top.index,i[0])              
-            for j in range(1,len(i)):
-                self.top.list_ctrl.SetStringItem(self.top.index, j, str(i[j]))
-            self.top.index += 1
+        if city == None:
+            for i in self.state_res:
+                self.top.list_ctrl.InsertStringItem(self.top.index,i[0])              
+                for j in range(1,len(i)):
+                    self.top.list_ctrl.SetStringItem(self.top.index, j, str(i[j]))
+                self.top.index += 1
+
+        else:
+            for i in self.res:
+                self.top.list_ctrl.InsertStringItem(self.top.index,i[0])              
+                for j in range(1,len(i)):
+                    self.top.list_ctrl.SetStringItem(self.top.index, j, str(i[j]))
+                self.top.index += 1
         
         #Display Results Window
         self.top.Show()
+        return None
                 
         
 
     def graph(self,event):
         #Open new .csv file...
         f = open('results.csv', 'wb')
+
         #Use CSV writer method to generate csv.
-        csvwriter = csv.writer(f)
-        for i in self.res:
-            for j in range(1,len(i)):
-                row = [i[0] ,str(i[j])]
-                csvwriter.writerow(row)
-        f.close()
-        
-        #R to generate graph from csv!
-        r = robjects.r
-        r('''
-                source('vis.r')
-        ''')
-        r_graph = robjects.globalenv['graph']
-        r_graph() 
+        if self.state == True:
+                    csvwriter = csv.writer(f)
+                    for i in self.state_res:
+                        for j in range(1,len(i)):
+                            row = [i[0] ,str(i[j])]
+                            csvwriter.writerow(row)
+                    f.close()
+                    
+                    #R to generate graph from csv!
+                    r = robjects.r
+                    r('''
+                            source('state_vis.r')
+                    ''')
+                    r_graph = robjects.globalenv['graph']
+                    r_graph() 
+                    return None
+
+        else:
+            csvwriter = csv.writer(f)
+            for i in self.res:
+                for j in range(1,len(i)):
+                    row = [i[0] ,str(i[j])]
+                    csvwriter.writerow(row)
+            f.close()
+            
+            #R to generate graph from csv!
+            r = robjects.r
+            r('''
+                    source('vis.r')
+            ''')
+            r_graph = robjects.globalenv['graph']
+            r_graph() 
+            return None
         
 
     def resClose(self,event):
         #Closes the results window
         # calling self.query(None) here results in a bug! Cannot run the program multiple times...
         self.top.Destroy()
+        return None
 
 
 def main():
---
+++ state_vis.r
@@ -0,0 +1,12 @@
+graph <- function() {
+  res = read.csv("results.csv", header=FALSE)
+  res = mutate(res, Percent = 100*(V2/(sum(res$V2))))
+  
+  if (nrow(res) >= 10){
+    res = head(res,10)
+  }
+  
+  p = ggplot(data=res, aes(x=V1, y=V2, fill=Percent)) + geom_bar(stat="identity") + coord_flip() + xlab('City') + ylab('Total Violations')
+  ggsave(file="state_graph.pdf")
+}
+graph()
